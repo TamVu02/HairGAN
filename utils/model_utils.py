a87e6ed9f20277c8fb94b8cc9fa22cfe94c7f0aa
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2024-01-08 16:16:12 +0700

    Update model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -4,7 +4,7 @@ from models.face_parsing.model import BiSeNet
 import pickle
 
 def load_base_models():
-    ckpt = "pretrained_models/ffhq.pkl"
+    ckpt = "/content/gdrive/MyDrive/HairGAN/tam_proposed/stylegan3-t-ffhq-1024x1024.pkl"
     #g_ema = Generator(1024, 512, 8)
     with open(ckpt, 'rb') as f:
         g_ema = pickle.load(f)['G_ema'].cuda()
@@ -12,7 +12,7 @@ def load_base_models():
 
     #mean_latent = torch.load(ckpt)["latent_avg"].unsqueeze(0).unsqueeze(0).repeat(1,18,1).clone().detach().cuda()
 
-    seg_pretrained_path = "pretrained_models/seg.pth"
+    seg_pretrained_path = "/content/gdrive/MyDrive/HairGAN/tam_proposed/seg.pth"
     seg = BiSeNet(n_classes=16)
     seg.load_state_dict(torch.load(seg_pretrained_path), strict=False)
     for param in seg.parameters():
