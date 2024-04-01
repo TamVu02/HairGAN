9a27d80b859c84d2313985ef6d4db61e8c92807e
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2024-04-01 16:52:19 +0700

    Update face_editor.py

--- editing/interfacegan/face_editor.py
+++ editing/interfacegan/face_editor.py
@@ -7,6 +7,8 @@ from configs.paths_config import interfacegan_aligned_edit_paths, interfacegan_u
 from models.stylegan3.model import GeneratorType
 from models.stylegan3.networks_stylegan3 import Generator
 from utils.common import tensor2im, generate_random_transform
+import face_alignment
+import torch.nn.functional as F
 
 
 class FaceEditor:
@@ -27,10 +29,14 @@ class FaceEditor:
             'pose': torch.from_numpy(np.load(paths['pose'])).cuda(),
         }
 
-    def edit(self, latents: torch.tensor, direction: str, factor: int = 1, factor_range: Optional[Tuple[int, int]] = None,
+    def edit(self, src_image: torch.tensor, latents: torch.tensor, direction: str, factor: int = 1, factor_range: Optional[Tuple[int, int]] = None,
              user_transforms: Optional[np.ndarray] = None, apply_user_transformations: Optional[bool] = False):
         edit_latents = []
         edit_images = []
+        diff_score=-100000000
+        if direction=='pose':
+          kp_source=self.get_kp_extractor(src_image)
+
         direction = self.interfacegan_directions[direction]
         if factor_range is not None:  # Apply a range of editing factors. for example, (-5, 5)
             for f in range(*factor_range):
@@ -38,8 +44,15 @@ class FaceEditor:
                 edit_image, user_transforms = self._latents_to_image(edit_latent,
                                                                      apply_user_transformations,
                                                                      user_transforms)
-                edit_latents.append(edit_latent)
-                edit_images.append(edit_image)
+                if direction=='pose':
+                  cur_diff_score = self.check_pose_diff(kp_source, edit_image, 'cuda')
+                  if cur_diff_score < diff_score:
+                    edit_images.append(edit_image)
+                    edit_latents.append(edit_latent)
+                    diff_score = cur_diff_score
+                else:
+                  edit_latents.append(edit_latent)
+                  edit_images.append(edit_image)
         else:
             edit_latents = latents + factor * direction
             edit_images, _ = self._latents_to_image(edit_latents, apply_user_transformations)
@@ -60,3 +73,12 @@ class FaceEditor:
             images = self.generator.synthesis(all_latents, noise_mode='const')
             images = [tensor2im(image) for image in images]
         return images, user_transforms
+
+    def get_kp_extractor(self, input_image):
+        return self.kp_extractor.face_alignment_net(((F.interpolate(input_image, size=(256, 256)) + 1) / 2).clamp(0, 1))
+
+    def check_pose_diff(self,kp1, im_2, device):
+        kp2 = self.get_kp_extractor(im_2)
+        kp_diff = np.mean(np.abs(kp1 - kp2))
+        return kp_diff
+
